66c7b70d4831a973827ec3d5d9332b25bbc72a59
pavan <pavangurram56@gmail.com>
2022-01-23 22:48:22 +0530

    changes made

--- Memory-Game/app.js
+++ Memory-Game/app.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll(".card");
 let cardOne, cardTwo;
+let matched = 0;
 function flipcard(e) {
   let clickedCard = e.target;
   if (clickedCard !== cardOne) {
@@ -14,13 +15,30 @@ function flipcard(e) {
       cardTwoImg = cardTwo.querySelector("img").src;
     matchedCards(cardOneImg, cardTwoImg);
   }
+  if (matched === 8) {
+  }
 }
 function matchedCards(img1, img2) {
   if (img1 === img2) {
-    return console.log("cards matched");
+    matched++;
+    console.log(matched);
+    cardOne.removeEventListener("click", flipcard);
+    cardTwo.removeEventListener("click", flipcard);
+    cardOne = cardTwo = "";
+    return;
   }
-  cardOne.classList.add("shake");
-  cardTwo.classList.add("shake");
+  //if two cards are not matched
+  setTimeout(() => {
+    //adding shake effect to cards after 400ms
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
+  setTimeout(() => {
+    //removing shake , flip effect  cards after 1200
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = "";
+  }, 1200);
 }
 cards.forEach((card) => {
   card.addEventListener("click", flipcard);
